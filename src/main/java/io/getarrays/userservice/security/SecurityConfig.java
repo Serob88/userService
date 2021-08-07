0762eccb1fe81a430de89d1dc5ca6ae12372bbeb
Kirakosyan Serob <serob.kirakosyan.88@gmail.com>
2021-08-07 19:36:30 +0400

    Added security filter, jwt token and refresh token functionality

--- src/main/java/io/getarrays/userservice/security/SecurityConfig.java
+++ src/main/java/io/getarrays/userservice/security/SecurityConfig.java
@@ -1,7 +1,14 @@
 package io.getarrays.userservice.security;
 
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+import io.getarrays.userservice.filter.CustomAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,6 +31,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    super.configure(http);
+    CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBeen());
+    customAuthenticationFilter.setFilterProcessesUrl("/api/login");
+
+    http.csrf().disable();
+    http.sessionManagement().sessionCreationPolicy(STATELESS);
+    http.authorizeRequests().antMatchers("/api/login/**").permitAll();
+    http.authorizeRequests().antMatchers(GET, "api/user/**").hasAnyAuthority("ROLE_USER");
+    http.authorizeRequests().antMatchers(POST, "api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
+    http.authorizeRequests().anyRequest().authenticated();
+    http.addFilter(customAuthenticationFilter);
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManagerBeen() throws Exception {
+    return super.authenticationManagerBean();
   }
 }
